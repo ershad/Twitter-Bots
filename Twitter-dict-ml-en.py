18abf56fe47281ad662a61a1e2c8c368d3567c14
Ershad K <ershad92@gmail.com>
2010-06-06 14:35:11 +0530

    Removed that comment PS: I'm learning git

--- Twitter-dict-ml-en.py
+++ Twitter-dict-ml-en.py
@@ -26,7 +26,7 @@ while (True):
 	time.sleep(sleep_time)
 	timeline = api.GetReplies()
 	for s in timeline:
-		#print "%s --> %s" % (s.user.name, s.text) #This line prints all the replies
+		#print "%s --> %s" % (s.user.name, s.text)
 		tweet = s.user.name + "\t" + s.text
 		y = tweet.find("dict")
 	
